9e9aa7ee2ca9af3ecfe20e02bb45822ef7064d21
EL-ATEIF Sara <sara.elateif@gmail.com>
2021-07-04 13:24:10 +0100

    Update workflow.py

--- workflow.py
+++ workflow.py
@@ -7,6 +7,10 @@ funcs = {}
 DATASET = 'train_enc'
 TST_DATASET = 'test_enc'
 LABELS =  'diabetes_mellitus'
+MODEL = 'lgbm_model'
+test_src = 'v3io:///projects/widsdb2/artifacts/raw_test_data.csv'
+train_src =  'v3io:///projects/widsdb2/artifacts/raw_train_data.csv'
+
 
 # Configure function resources and local settings
 def init_functions(functions: dict, project=None, secrets=None):
@@ -21,7 +25,7 @@ def init_functions(functions: dict, project=None, secrets=None):
                 
 )
 
-def kfpipeline(source_url='store://raw_train_data', test_url='store://raw_test_data'):
+def kfpipeline(source_url=train_src, test_url=test_src):
 
     # Ingest the data set
     ingest = funcs['prep'].as_step(
@@ -37,7 +41,7 @@ def kfpipeline(source_url='store://raw_train_data', test_url='store://raw_test_d
         inputs={'src': test_url},
         outputs=[TST_DATASET])
     
-        # Train a model   
+      # Train a model   
     train = funcs["train-wids"].as_step(
         name="train-wids",
         params={"label_column": LABELS},
@@ -46,12 +50,9 @@ def kfpipeline(source_url='store://raw_train_data', test_url='store://raw_test_d
   
      # Deploy the model as a serverless function
     deploy = funcs["lightgbm-serving"].deploy_step(
-        models={f"{DATASET}_v1": train.outputs['model']})
+        models={f"{MODEL}_v1": train.outputs['model']})
    
-   # test out new model server (via REST API calls)
-    #tester = funcs["live_tester"].as_step(name='model-tester',
-    #    params={'addr': deploy.outputs['endpoint'], 'model': f"{DATASET}:v1", 'label_column':LABELS},
-    #   inputs={'table': train.outputs['test_set']})
-    
-    
-    ## Run the inference with the serving function
+    #test out new model server (via REST API calls)
+    tester = funcs["live_tester"].as_step(name='model-tester',
+        params={'addr': deploy.outputs['endpoint'], 'model': f"{MODEL}_v1", 'label_column':LABELS},
+        inputs={'table': train.outputs['test_set']})
